67bdf1937c27c29ff411b2e92dea555202d8f8ee
Scarsz <truescarsz@gmail.com>
2020-11-20 13:30:37 -0800

    get rid of unnecessary message

--- cmd/post.go
+++ cmd/post.go
@@ -4,12 +4,9 @@ import (
 	"fmt"
 	"github.com/Scarsz/bincli/bin"
 	"github.com/spf13/cobra"
-	"strings"
 )
 
 func Post(cmd *cobra.Command, args []string) {
-	fmt.Println("Creating bin from files: " + strings.Join(args, ", "))
-
 	var files []bin.File
 	for _, fileName := range args {
 		files = append(files, bin.FileFromFileName(fileName))
