24e9c6795850c6df96384e4687c607e9156639da
Scarsz <truescarsz@gmail.com>
2020-11-20 13:30:11 -0800

    add support for piping text into paste command

--- cmd/paste.go
+++ cmd/paste.go
@@ -1,15 +1,39 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/Scarsz/bincli/bin"
 	"github.com/spf13/cobra"
+	"os"
 	"strings"
 )
 
 func Paste(cmd *cobra.Command, args []string) {
-	content := strings.Join(args, " ")
-	fmt.Println("Creating bin from text: " + content)
+	var content string
+
+	fi, err := os.Stdin.Stat()
+	if err != nil {
+		panic(err)
+	}
+	if fi.Mode()&os.ModeNamedPipe == 0 {
+		// no piped input, use args instead
+		if len(args) == 0 {
+			fmt.Println("ERROR: No text given to paste")
+			return
+		} else {
+			content = strings.Join(args, " ")
+		}
+	} else {
+		// have piped input
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			content += scanner.Text() + "\n"
+		}
+		if err := scanner.Err(); err != nil {
+			panic(err)
+		}
+	}
 
 	files := []bin.File{bin.FileFromText("cli.txt", content, "")}
 
--- main.go
+++ main.go
@@ -10,31 +10,30 @@ func main() {
 	rootCmd := &cobra.Command{Use: "bin"}
 
 	cmdInfo := &cobra.Command{
-		Use: "info <id/url>",
+		Use:   "info <id/url>",
 		Short: "Get information about a bin",
-		Args: cobra.MinimumNArgs(1),
-		Run: cmd.Info,
+		Args:  cobra.MinimumNArgs(1),
+		Run:   cmd.Info,
 	}
 
 	// uploading commands
 	cmdGet := &cobra.Command{
-		Use: "get <url>",
+		Use:   "get <url>",
 		Short: "Download a bin",
-		Long: "Download a bin from the given URL",
-		Args: cobra.MinimumNArgs(1),
-		Run: cmd.Get,
+		Long:  "Download a bin from the given URL",
+		Args:  cobra.MinimumNArgs(1),
+		Run:   cmd.Get,
 	}
 	cmdPost := &cobra.Command{
-		Use: "post <file>...",
+		Use:   "post <file>...",
 		Short: "Create a new bin from the given files",
-		Args: cobra.MinimumNArgs(1),
-		Run: cmd.Post,
+		Args:  cobra.MinimumNArgs(1),
+		Run:   cmd.Post,
 	}
 	cmdPaste := &cobra.Command{
-		Use: "paste <text>",
+		Use:   "paste <text>",
 		Short: "Create a new bin with the given text",
-		Args: cobra.MinimumNArgs(1),
-		Run: cmd.Paste,
+		Run:   cmd.Paste,
 	}
 
 	rootCmd.AddCommand(cmdInfo, cmdGet, cmdPost, cmdPaste)
